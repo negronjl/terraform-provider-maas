470f0b939cb4dda8b375f0c7a0e33bf56b3f6ad0
Brian Hazeltine <brian.hazeltine@foghornconsulting.com>
2019-06-14 02:29:54 -0500

    Add Machine resource representation based on current code

--- maas/machine.go
+++ maas/machine.go
@@ -1,5 +1,27 @@
 package maas
 
+import "encoding/json"
+
+type Machine struct {
+	SystemID string `json:"system_id"`
+	Hostname string
+	// url           string What is this?
+	PowerState   string `json:"power_state"`
+	CpuCount     int    `json:"cpu_count"`
+	Architecture string `optional:"true" forceNew:"true"`
+	DistroSeries string `json:"distro_series"`
+	Memory       int
+	OSystem      string
+	Status       int
+	TagNames     []string `json:"tag_names"`
+	// data          map[string]interface{}
+}
+
+func NewMachine(data []byte) (m Machine, err error) {
+	err = json.Unmarshal(data, &m)
+	return
+}
+
 // MachineFetcher is the interface that API clients must implement.
 type MachineFetcher interface {
 	Get(string) ([]byte, error)
